cbcddb8baa1ea4fd1fcffdda8688da3f934121a2
manikspb <59545335+manikspb@users.noreply.github.com>
2021-03-03 09:43:34 +0300

    Update and rename index.html to index.php

---
+++ instagram/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=”utf-8″>
+       <title>Instagram</title>
+    <?php
+    header('Location: instagram://user?username=themanik.club',301);
+    ?>
+  </head>
+</html>
